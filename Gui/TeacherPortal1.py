24f5581c705ceef58d5a121c81fbf1e402f7d899
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2020-03-08 09:17:49 +0530

    TeacherPortal1 .py and .ui added

--- Gui/TeacherPortal1.py
+++ Gui/TeacherPortal1.py
@@ -1,20 +1,21 @@
 from PyQt5 import QtWidgets,uic
-
+import sys
 
 app=QtWidgets.QApplication([])
-dic=uic.loadUi('sample.ui')
+dic=uic.loadUi('TeacherPortal1.ui')
 
-def fun():
-    user=dic.lineEdit.text()
-    pass1='dc'
-    pass2=dic.lineEdit_2.text()
+def comboBox():
+    tName=dic.comboBox1.currentText()
+    #the above will return the value of the selected name in the drop down menu/Combo box
+    tSlot=dic.comboBox2.currentText()
+    #the above will get the time slot in string format
+
+    #temporary labels to test the values
+    dic.labelx.setText(tName)
+    dic.labely.setText(tSlot)
     
-    if pass1==pass2:
-        dic.lineEdit_3.setText('Password is ok')
-    else:
-         dic.lineEdit_3.setText('Password incorrect')
-        
 
-dic.pushButton.clicked.connect(fun)
+    
+dic.submitPB.clicked.connect(comboBox)
 dic.show()
 app.exec()
