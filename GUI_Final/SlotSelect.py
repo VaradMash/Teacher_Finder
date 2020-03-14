da58f7c8f9b67b4ed0935ec02a416ebac1cb5feb
VaradMash <varadmash2201@gmail.com>
2020-03-14 15:27:40 +0530

    Code Updated'

--- GUI_Final/SlotSelect.py
+++ GUI_Final/SlotSelect.py
@@ -1,15 +1,21 @@
 from xlrd import *
 import os as o
 from tkinter import *
+import VerifyLeave as v
 
 def get_slot(name,slot_no,day):
-    name=name+'.xlsx'
-    p=open_workbook(name)
+    ename=name+'.xlsx'
+    p=open_workbook(ename)
     s=p.sheet_by_index(0)
     x=slot_no+2
-    y=day+2
-    v=s.cell_value(x,y)
-    return str(v)
+    y=day+1
+    u=s.cell_value(x,y)
+    ret=v.leaverec(name)
+    if ret==True:
+        return "Faculty available today",str(u)
+    else:
+        return "Faculty on leave today",str(u)
 
 
   
+get_slot('NVS',2,1)
