8ce3d1ef7c15cdf6ecb50580f74d8c623ea74f70
VaradMash <varadmash2201@gmail.com>
2020-03-18 15:55:25 +0530

    If statement for homeroom added

--- GUI_Final/SlotSelect.py
+++ GUI_Final/SlotSelect.py
@@ -9,8 +9,18 @@ def get_slot(name,slot_no,day):
     s=p.sheet_by_index(0)
     x=slot_no+2
     y=day+1
-    u=s.cell_value(x,y)
+    
     ret=v.leaverec(name)
+    if slot_no==1 and day!=0:
+        u=s.cell_value(1,1)
+    elif slot_no!=0 and day!=0:
+        u=s.cell_value(x,y)
+    else :
+        u="Please Select Proper Day and Slot"
+
+    
+
+    
     if ret==True:
         return "Faculty available today",str(u)
     else:
